736d20b09b38452467af0d5479c1bc0f45522df7
Allan Segovia Spadini <allan.spadini@gmail.com>
2025-03-23 17:26:05 -0300

    server

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ def main():
         title="Classificação de Folhas de Manga",
         description="Faça o upload de uma imagem de folha de manga e receba a previsão do modelo."
     )
-    interface.launch(share=True, server_port=8080)
+    interface.launch(share=True, server_name="0.0.0.0",server_port=8080)
 
 if __name__ == "__main__":
     main()
